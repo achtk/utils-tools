1dc3ccb16d6b4c602b27db09d0ebbead9b860456
ACHTK <yemengzhidiandig@gmail.com>
2020-09-26 10:01:36 +0800

    添加异常对象

---
+++ utils-tools/src/main/java/com/chua/utils/tools/exceptions/NoImplementationException.java
@@ -0,0 +1,9 @@
+package com.chua.utils.tools.exceptions;
+
+/**
+ * 未实现异常
+ * @author CH
+ * @date 2020-09-26
+ */
+public class NoImplementationException extends Exception {
+}
---
+++ utils-tools/src/main/java/com/chua/utils/tools/exceptions/NotSupportedException.java
@@ -0,0 +1,9 @@
+package com.chua.utils.tools.exceptions;
+
+/**
+ * 不支持异常
+ * @author CH
+ * @date 2020-09-26
+ */
+public class NotSupportedException extends Exception {
+}
