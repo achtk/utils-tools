ca226e8148511c8ac983a5e493473300ac38c971
ACHTK <yemengzhidiandig@gmail.com>
2020-09-28 11:14:44 +0800

    jedis模块

---
+++ utils-netx-extension-redis-jedis/src/main/java/com/chua/utils/netx/redis/jedis/context/JedisContext.java
@@ -0,0 +1,302 @@
+package com.chua.utils.netx.redis.jedis.context;
+
+import com.chua.unified.properties.NetxProperties;
+import com.chua.utils.netx.redis.jedis.factory.JedisFactory;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import redis.clients.jedis.ShardedJedis;
+import redis.clients.jedis.ShardedJedisPool;
+
+import java.io.*;
+
+/**
+ * jedis上下文
+ *
+ * @author CH
+ * @date 2020-09-28
+ */
+@Slf4j
+public class JedisContext implements AutoCloseable {
+
+    private final JedisFactory netxFactory;
+    private final ShardedJedisPool shardedJedisPool;
+    private NetxProperties netxProperties;
+
+    public JedisContext(@NonNull NetxProperties netxProperties) {
+        this.netxProperties = netxProperties;
+        this.netxFactory = new JedisFactory(netxProperties);
+        this.netxFactory.start();
+        this.shardedJedisPool = this.netxFactory.client();
+    }
+
+    /**
+     * 将对象-->byte[] (由于jedis中不支持直接存储object所以转换成byte[]存入)
+     *
+     * @param object
+     * @return
+     */
+    private byte[] serialize(Object object) {
+        ObjectOutputStream oos = null;
+        ByteArrayOutputStream baos = null;
+        try {
+            // 序列化
+            baos = new ByteArrayOutputStream();
+            oos = new ObjectOutputStream(baos);
+            oos.writeObject(object);
+            byte[] bytes = baos.toByteArray();
+            return bytes;
+        } catch (Exception e) {
+            log.error(e.getMessage(), e);
+        } finally {
+            try {
+                oos.close();
+                baos.close();
+            } catch (IOException e) {
+                log.error(e.getMessage(), e);
+            }
+        }
+        return null;
+    }
+
+    /**
+     * 将byte[] -->Object
+     *
+     * @param bytes
+     * @return
+     */
+    private Object unserialize(byte[] bytes) {
+        ByteArrayInputStream bais = null;
+        try {
+            // 反序列化
+            bais = new ByteArrayInputStream(bytes);
+            ObjectInputStream ois = new ObjectInputStream(bais);
+            return ois.readObject();
+        } catch (Exception e) {
+            log.error(e.getMessage(), e);
+        } finally {
+            try {
+                bais.close();
+            } catch (IOException e) {
+                log.error(e.getMessage(), e);
+            }
+        }
+        return null;
+    }
+
+    // ------------------------ jedis util ------------------------
+    /**
+     * 存储简单的字符串或者是Object 因为jedis没有分装直接存储Object的方法，所以在存储对象需斟酌下
+     * 存储对象的字段是不是非常多而且是不是每个字段都用到，如果是的话那建议直接存储对象，
+     * 否则建议用集合的方式存储，因为redis可以针对集合进行日常的操作很方便而且还可以节省空间
+     */
+
+    /**
+     * Set String
+     *
+     * @param key 索引
+     * @param value
+     * @param seconds 存活时间,单位/秒
+     * @return
+     */
+    public String setStringValue(String key, String value, int seconds) {
+        String result = null;
+        ShardedJedis client = shardedJedisPool.getResource();
+        try {
+            result = client.setex(key, seconds, value);
+        } catch (Exception e) {
+            log.error(e.getMessage(), e);
+        } finally {
+            if (client != null) {
+                client.close();
+            }
+        }
+        return result;
+    }
+
+    /**
+     * Set Object
+     *
+     * @param key 索引
+     * @param obj
+     * @param seconds 存活时间,单位/秒
+     */
+    public String setObjectValue(String key, Object obj, int seconds) {
+        String result = null;
+        ShardedJedis client = shardedJedisPool.getResource();
+        try {
+            result = client.setex(key.getBytes(), seconds, serialize(obj));
+        } catch (Exception e) {
+            log.error(e.getMessage(), e);
+        } finally {
+            if (client != null) {
+                client.close();
+            }
+        }
+        return result;
+    }
+
+    /**
+     * Get String
+     *
+     * @param key 索引
+     * @return
+     */
+    public String getStringValue(String key) {
+        String value = null;
+        ShardedJedis client = shardedJedisPool.getResource();
+        try {
+            value = client.get(key);
+        } catch (Exception e) {
+            log.error(e.getMessage(), e);
+        } finally {
+            if (client != null) {
+                client.close();
+            }
+        }
+        return value;
+    }
+
+    /**
+     * Get Object
+     *
+     * @param key 索引
+     * @return
+     */
+    public Object getObjectValue(String key) {
+        Object obj = null;
+        ShardedJedis client = shardedJedisPool.getResource();
+        try {
+            byte[] bytes = client.get(key.getBytes());
+            if (bytes != null && bytes.length > 0) {
+                obj = unserialize(bytes);
+            }
+        } catch (Exception e) {
+            log.error(e.getMessage(), e);
+        } finally {
+            if (client != null) {
+                client.close();
+            }
+        }
+        return obj;
+    }
+
+    /**
+     * Delete key
+     *
+     * @param key 索引
+     * @return Integer reply, specifically:
+     * an integer greater than 0 if one or more keys were removed
+     * 0 if none of the specified key existed
+     */
+    public Long del(String key) {
+        Long result = null;
+        ShardedJedis client = shardedJedisPool.getResource();
+        try {
+            result = client.del(key);
+        } catch (Exception e) {
+            log.error(e.getMessage(), e);
+        } finally {
+            if (client != null) {
+                client.close();
+            }
+        }
+        return result;
+    }
+
+    /**
+     * incrBy i(+i)
+     *
+     * @param key 索引
+     * @param i 序列
+     * @return new value after incr
+     */
+    public Long incrBy(String key, int i) {
+        Long result = null;
+        ShardedJedis client = shardedJedisPool.getResource();
+        try {
+            result = client.incrBy(key, i);
+        } catch (Exception e) {
+            log.error(e.getMessage(), e);
+        } finally {
+            if (client != null) {
+                client.close();
+            }
+        }
+        return result;
+    }
+
+    /**
+     * exists valid
+     *
+     * @param key 索引
+     * @return Boolean reply, true if the key exists, otherwise false
+     */
+    public boolean exists(String key) {
+        Boolean result = null;
+        ShardedJedis client = shardedJedisPool.getResource();
+        try {
+            result = client.exists(key);
+        } catch (Exception e) {
+            log.error(e.getMessage(), e);
+        } finally {
+            if (client != null) {
+                client.close();
+            }
+        }
+        return result;
+    }
+
+    /**
+     * expire reset
+     *
+     * @param key 索引
+     * @param seconds 存活时间,单位/秒
+     * @return Integer reply, specifically:
+     * 1: the timeout was set.
+     * 0: the timeout was not set since the key already has an associated timeout (versions lt 2.1.3), or the key does not exist.
+     */
+    public long expire(String key, int seconds) {
+        Long result = null;
+        ShardedJedis client = shardedJedisPool.getResource();
+        try {
+            result = client.expire(key, seconds);
+        } catch (Exception e) {
+            log.error(e.getMessage(), e);
+        } finally {
+            if (client != null) {
+                client.close();
+            }
+        }
+        return result;
+    }
+
+    /**
+     * expire at unixTime
+     *
+     * @param key 索引
+     * @param unixTime 超时时间
+     * @return
+     */
+    public long expireAt(String key, long unixTime) {
+        Long result = null;
+        ShardedJedis client = shardedJedisPool.getResource();
+        try {
+            result = client.expireAt(key, unixTime);
+        } catch (Exception e) {
+            log.error(e.getMessage(), e);
+        } finally {
+            if (client != null) {
+                client.close();
+            }
+        }
+        return result;
+    }
+
+    @Override
+    public void close() throws Exception {
+        if (null != this.netxFactory) {
+            netxFactory.close();
+        }
+    }
+}
---
+++ utils-netx-extension-redis-jedis/src/main/java/com/chua/utils/netx/redis/jedis/factory/JedisFactory.java
@@ -0,0 +1,116 @@
+package com.chua.utils.netx.redis.jedis.factory;
+
+import com.chua.unified.properties.NetxProperties;
+import com.chua.utils.netx.factory.INetxFactory;
+import com.chua.utils.tools.common.MapHelper;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import redis.clients.jedis.JedisPoolConfig;
+import redis.clients.jedis.JedisShardInfo;
+import redis.clients.jedis.ShardedJedis;
+import redis.clients.jedis.ShardedJedisPool;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * jedis
+ * @author CH
+ * @date 2020-09-28
+ */
+@Slf4j
+@RequiredArgsConstructor
+public class JedisFactory implements INetxFactory<ShardedJedisPool> {
+
+    @NonNull
+    private NetxProperties netxProperties;
+    private ShardedJedisPool shardedJedisPool;
+    private static ReentrantLock INSTANCE_INIT_LOCL = new ReentrantLock(false);
+
+    @Override
+    public void configure(NetxProperties netxProperties) {
+        this.netxProperties = netxProperties;
+    }
+
+    @Override
+    public ShardedJedisPool client() {
+        return shardedJedisPool;
+    }
+
+    @Override
+    public void start() {
+        if (shardedJedisPool != null) {
+            return;
+        }
+        try {
+            if (INSTANCE_INIT_LOCL.tryLock(2, TimeUnit.SECONDS)) {
+                try {
+                    initialJedis();
+                } finally {
+                    INSTANCE_INIT_LOCL.unlock();
+                }
+            }
+
+        } catch (InterruptedException e) {
+            log.error(e.getMessage(), e);
+        }
+
+        if (shardedJedisPool == null) {
+            throw new NullPointerException(">>>>>>>>>>> xxl-sso, JedisUtil.ShardedJedisPool is null.");
+        }
+    }
+
+    /**
+     * 初始化Jedis
+     */
+    private synchronized void initialJedis() {
+        if (shardedJedisPool == null) {
+            // JedisPoolConfig
+            JedisPoolConfig config = new JedisPoolConfig();
+            config.setMaxTotal(MapHelper.ints(NetxProperties.CONFIG_FIELD_MAX_CONNECTION, 200, netxProperties));
+            config.setMaxIdle(50);
+            config.setMinIdle(8);
+            // 获取连接时的最大等待毫秒数(如果设置为阻塞时BlockWhenExhausted),如果超时就抛异常, 小于零:阻塞不确定的时间,  默认-1
+            config.setMaxWaitMillis(10000);
+            // 在获取连接的时候检查有效性, 默认false
+            config.setTestOnBorrow(true);
+            // 调用returnObject方法时，是否进行有效检查
+            config.setTestOnReturn(false);
+            // Idle时进行连接扫描
+            config.setTestWhileIdle(true);
+            // 表示idle object evitor两次扫描之间要sleep的毫秒数
+            config.setTimeBetweenEvictionRunsMillis(30000);
+            // 表示idle object evitor每次扫描的最多的对象数
+            config.setNumTestsPerEvictionRun(10);
+            // 表示一个对象至少停留在idle状态的最短时间，然后才能被idle object evitor扫描并驱逐；这一项只有在timeBetweenEvictionRunsMillis大于0时才有意义
+            config.setMinEvictableIdleTimeMillis(60000);
+
+            String[] addressArr = netxProperties.getHost();
+
+            // JedisShardInfo List
+            List<JedisShardInfo> jedisShardInfos = new LinkedList<JedisShardInfo>();
+
+            for (int i = 0; i < addressArr.length; i++) {
+                JedisShardInfo jedisShardInfo = new JedisShardInfo(addressArr[i]);
+                jedisShardInfos.add(jedisShardInfo);
+            }
+            this.shardedJedisPool = new ShardedJedisPool(config, jedisShardInfos);
+            log.info(">>>>>>>>>>> sso, JedisUtil.ShardedJedisPool init success.");
+        }
+    }
+
+    @Override
+    public boolean isStart() {
+        return null != shardedJedisPool && !shardedJedisPool.isClosed();
+    }
+
+    @Override
+    public void close() throws Exception {
+        if (shardedJedisPool != null) {
+            shardedJedisPool.close();
+        }
+    }
+}
