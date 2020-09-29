7251000909903a531e33311fda21bd11d4bf1bf6
ACHTK <yemengzhidiandig@gmail.com>
2020-09-29 17:23:10 +0800

    fegin

--- utils-tools-extension-fegin/src/main/java/com/chua/utils/tools/fegin/FeginRemoteCallFactory.java
+++ utils-tools-extension-fegin/src/main/java/com/chua/utils/tools/fegin/FeginRemoteCallFactory.java
@@ -1,8 +1,14 @@
 package com.chua.utils.tools.fegin;
 
+import com.chua.unified.configure.ServerConfig;
 import com.chua.unified.interfaces.IRemoteCallFactory;
 import com.chua.utils.tools.fegin.helper.FeginHelper;
 import feign.Feign;
+import feign.Request;
+import feign.Retryer;
+import feign.Target;
+import feign.jackson.JacksonDecoder;
+import feign.jackson.JacksonEncoder;
 
 /**
  * Fegin远程调用工厂
@@ -11,7 +17,12 @@ import feign.Feign;
 public class FeginRemoteCallFactory implements IRemoteCallFactory {
 
     @Override
-    public <T> T target(Class<T> tClass, String serverUrl) {
-        return FeginHelper.getObjectForEntity(tClass, serverUrl);
+    public <T> T target(Class<T> tClass, String serverUrl, ServerConfig serverConfig) {
+        Feign.Builder builder = Feign.builder()
+                .encoder(new JacksonEncoder())
+                .decoder(new JacksonDecoder())
+                .options(new Request.Options(serverConfig.getConnectTimeoutMillis(1000), serverConfig.getReadTimeoutMillis(3500)))
+                .retryer(new Retryer.Default(100, 2000, serverConfig.getRetry(3)));
+        return builder.target(tClass, serverUrl);
     }
 }
