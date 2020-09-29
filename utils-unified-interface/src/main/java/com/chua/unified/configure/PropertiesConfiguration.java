8c4750134e4313532927d30e616e454797afcd52
ACHTK <yemengzhidiandig@gmail.com>
2020-09-29 11:11:01 +0800

    map接口

---
+++ utils-unified-interface/src/main/java/com/chua/unified/configure/PropertiesConfiguration.java
@@ -0,0 +1,26 @@
+package com.chua.unified.configure;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * properties基础配置
+ * @author CH
+ * @date 2020-09-29
+ */
+@Getter
+@Setter
+public class PropertiesConfiguration {
+    /**
+     * 类型不一致的强制转换
+     */
+    private boolean typeInconsistentForcedConversion = true;
+    /**
+     * 当数据为空是否需要空集合
+     */
+    private boolean needEmptyCollection = false;
+    /**
+     * 集合忽略null
+     */
+    private boolean collectionIgnoresNull = true;
+}
