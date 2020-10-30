78d501fa31bf6e9bd8126650797699daf7720e87
ACHTK <yemengzhidiandig@gmail.com>
2020-10-30 09:09:17 +0800

    删除无用的包

--- utils-netx-extension-zookeeper/src/main/java/com/chua/utils/netx/zookeeper/center/ZookeeperConfigurationCenter.java
+++ utils-netx-extension-zookeeper/src/main/java/com/chua/utils/netx/zookeeper/center/ZookeeperConfigurationCenter.java
@@ -7,18 +7,17 @@ import com.chua.utils.netx.zookeeper.context.ZookeeperContext;
 import com.chua.utils.tools.common.ByteHelper;
 import com.chua.utils.tools.common.JsonHelper;
 import com.chua.utils.tools.common.StringHelper;
-import com.chua.utils.tools.function.IConsumer;
 import com.chua.utils.tools.function.producer.NetxPropertiesProducer;
 import com.chua.utils.tools.properties.NetxProperties;
 import com.chua.utils.tools.strategy.handler.IStrategyPolicy;
 import com.chua.utils.tools.strategy.helper.StrategyHelper;
 import com.google.common.base.Strings;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.curator.framework.recipes.cache.ChildData;
-import org.apache.curator.framework.recipes.cache.TreeCacheEvent;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -40,8 +39,9 @@ public class ZookeeperConfigurationCenter implements EventPublishingConfigServic
     private static final String DEFAULT_CONFIG_NODE_NAME = "/center/config";
     private static final String CONFIG_NODE_NAME = "config";
     private static final String DEFAULT_GROUP = "group";
-    private static final String CLUSER_NAME_DEFAULT_GROUP = "cluserName";
+    private static final String CLUSTER_NAME_DEFAULT_GROUP = "cluserName";
     @NonNull
+    @Getter
     private NetxProperties netxProperties;
     private ZookeeperContext zookeeperContext;
     private String configName;
@@ -89,18 +89,10 @@ public class ZookeeperConfigurationCenter implements EventPublishingConfigServic
     @Override
     public void addListener(String dataId, String group, Listener listener) throws Exception {
         String cacheKey = createCacheKey(dataId, group);
-        zookeeperContext.addListener(cacheKey, new IConsumer<TreeCacheEvent>() {
-            @Override
-            public void next(TreeCacheEvent item) {
-                Executor executor = listener.getExecutor();
-                ChildData childData = item.getData();
-                executor.execute(new Runnable() {
-                    @Override
-                    public void run() {
-                        listener.receiveConfigInfo(ByteHelper.toString(childData.getData()));
-                    }
-                });
-            }
+        zookeeperContext.addListener(cacheKey, item -> {
+            Executor executor = listener.getExecutor();
+            ChildData childData = item.getData();
+            executor.execute(() -> listener.receiveConfigInfo(ByteHelper.toString(childData.getData())));
         });
     }
 
@@ -121,9 +113,9 @@ public class ZookeeperConfigurationCenter implements EventPublishingConfigServic
     /**
      * 创建索引
      *
-     * @param dataId
-     * @param group
-     * @return
+     * @param dataId 数据标识
+     * @param group  分组
+     * @return String
      */
     private String createCacheKey(String dataId, String group) {
         group = StringHelper.defaultIfBlank(group, DEFAULT_GROUP);
@@ -133,12 +125,12 @@ public class ZookeeperConfigurationCenter implements EventPublishingConfigServic
     /**
      * 创建索引
      *
-     * @param dataId
-     * @param group
-     * @return
+     * @param dataId 数据标识
+     * @param group  分组
+     * @return String
      */
     private String createCacheKey(String cluserName, String dataId, String group) {
-        cluserName = StringHelper.defaultIfBlank(cluserName, CLUSER_NAME_DEFAULT_GROUP);
+        cluserName = StringHelper.defaultIfBlank(cluserName, CLUSTER_NAME_DEFAULT_GROUP);
         String newKey = configName + "/" + cluserName + "/" + dataId;
         return null == group ? newKey : newKey + "/" + group;
     }
@@ -150,7 +142,7 @@ public class ZookeeperConfigurationCenter implements EventPublishingConfigServic
             return false;
         }
         String newKey = createCacheKey(dataId, group);
-        return !zookeeperContext.exist(newKey) ? true : zookeeperContext.delete(newKey);
+        return !zookeeperContext.exist(newKey) || zookeeperContext.delete(newKey);
     }
 
     @Override
@@ -159,18 +151,10 @@ public class ZookeeperConfigurationCenter implements EventPublishingConfigServic
         if (!zookeeperContext.exist(newKey)) {
             return;
         }
-        zookeeperContext.removeListener(newKey, new IConsumer<TreeCacheEvent>() {
-            @Override
-            public void next(TreeCacheEvent item) {
-                Executor executor = listener.getExecutor();
-                ChildData childData = item.getData();
-                executor.execute(new Runnable() {
-                    @Override
-                    public void run() {
-                        listener.receiveConfigInfo(ByteHelper.toString(childData.getData()));
-                    }
-                });
-            }
+        zookeeperContext.removeListener(newKey, item -> {
+            Executor executor = listener.getExecutor();
+            ChildData childData = item.getData();
+            executor.execute(() -> listener.receiveConfigInfo(ByteHelper.toString(childData.getData())));
         });
     }
 
@@ -226,7 +210,7 @@ public class ZookeeperConfigurationCenter implements EventPublishingConfigServic
 
     @Override
     public List<Map<String, Object>> selectInstances(String serviceName, boolean healthy) throws Exception {
-        String cacheKey = createCacheKey(CLUSER_NAME_DEFAULT_GROUP, serviceName, null);
+        String cacheKey = createCacheKey(CLUSTER_NAME_DEFAULT_GROUP, serviceName, null);
         if (!zookeeperContext.exist(cacheKey)) {
             return null;
         }
@@ -251,36 +235,20 @@ public class ZookeeperConfigurationCenter implements EventPublishingConfigServic
     @Override
     public <T> void subscribe(String serviceName, Listener<T> listener) throws Exception {
         String cacheKey = createCacheKey(null, serviceName, null);
-        zookeeperContext.addListener(cacheKey, new IConsumer<TreeCacheEvent>() {
-            @Override
-            public void next(TreeCacheEvent item) {
-                Executor executor = listener.getExecutor();
-                ChildData childData = item.getData();
-                executor.execute(new Runnable() {
-                    @Override
-                    public void run() {
-                        listener.receiveConfigInfo(ByteHelper.toString(childData.getData()));
-                    }
-                });
-            }
+        zookeeperContext.addListener(cacheKey, item -> {
+            Executor executor = listener.getExecutor();
+            ChildData childData = item.getData();
+            executor.execute(() -> listener.receiveConfigInfo(ByteHelper.toString(childData.getData())));
         });
     }
 
     @Override
     public <T> void unsubscribe(String serviceName, Listener<T> listener) throws Exception {
         String cacheKey = createCacheKey(null, serviceName, null);
-        zookeeperContext.removeListener(cacheKey, new IConsumer<TreeCacheEvent>() {
-            @Override
-            public void next(TreeCacheEvent item) {
-                Executor executor = listener.getExecutor();
-                ChildData childData = item.getData();
-                executor.execute(new Runnable() {
-                    @Override
-                    public void run() {
-                        listener.receiveConfigInfo(ByteHelper.toString(childData.getData()));
-                    }
-                });
-            }
+        zookeeperContext.removeListener(cacheKey, item -> {
+            Executor executor = listener.getExecutor();
+            ChildData childData = item.getData();
+            executor.execute(() -> listener.receiveConfigInfo(ByteHelper.toString(childData.getData())));
         });
     }
 }
