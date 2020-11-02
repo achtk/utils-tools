bdfc47e04cba7dd4b842f14c1651e8ab41de5e14
ACHTK <yemengzhidiandig@gmail.com>
2020-11-02 16:49:29 +0800

    lucene template

--- utils-netx-extension-lucene/src/main/java/com/chua/utils/netx/lucene/aware/MemoryLuceneContextAware.java
+++ utils-netx-extension-lucene/src/main/java/com/chua/utils/netx/lucene/aware/MemoryLuceneContextAware.java
@@ -1,8 +1,12 @@
 package com.chua.utils.netx.lucene.aware;
 
+import lombok.NoArgsConstructor;
+import org.apache.lucene.store.Directory;
+import org.apache.lucene.store.NoLockFactory;
 import org.apache.lucene.store.RAMDirectory;
 
 import java.io.IOException;
+import java.nio.file.Path;
 
 /**
  * 内存模式启动lucene
@@ -17,4 +21,11 @@ public class MemoryLuceneContextAware extends NioFSLuceneContextAware {
         super(null, new RAMDirectory());
     }
 
+    public MemoryLuceneContextAware(Path path, Directory directory) throws IOException {
+        super(path, directory);
+    }
+
+    public static MemoryLuceneContextAware newNoLockFactory() throws IOException {
+        return new MemoryLuceneContextAware(null, new RAMDirectory(NoLockFactory.INSTANCE));
+    }
 }
