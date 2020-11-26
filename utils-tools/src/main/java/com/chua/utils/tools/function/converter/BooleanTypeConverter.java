34faf5860e9823767f75c2f212b7ecd0147a9cfa
ACHTK <yemengzhidiandig@gmail.com>
2020-11-26 15:14:28 +0800

    添加 BooleanTypeConverter

---
+++ utils-tools/src/main/java/com/chua/utils/tools/function/converter/BooleanTypeConverter.java
@@ -0,0 +1,34 @@
+package com.chua.utils.tools.function.converter;
+
+/**
+ * boolean类型转化
+ *
+ * @author CH
+ * @version 1.0.0
+ * @since 2020/11/26
+ */
+public class BooleanTypeConverter implements TypeConverter<Boolean> {
+    @Override
+    public Boolean convert(Object value) {
+        if (null == value) {
+            return null;
+        }
+
+        if (value instanceof Boolean) {
+            return (Boolean) value;
+        }
+
+        if (value instanceof String) {
+            try {
+                return Boolean.valueOf(value.toString());
+            } catch (Exception e) {
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public Class<? extends Boolean> getType() {
+        return null;
+    }
+}
