da318e1d92151a3ffb76d118b598256d5a04ba57
ACHTK <yemengzhidiandig@gmail.com>
2021-01-26 16:37:13 +0800

    1、[优化]描述

---
+++ utils-tools/src/main/java/com/chua/utils/tools/util/IdUtils.java
@@ -0,0 +1,13 @@
+package com.chua.utils.tools.util;
+
+import com.chua.utils.tools.text.IdHelper;
+
+/**
+ * id工具类
+ *
+ * @author CH
+ * @version 1.0.0
+ * @since 2021/1/26
+ */
+public class IdUtils extends IdHelper {
+}
