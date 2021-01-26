99bea4962582db59714015fb21ab5269e7840dd9
ACHTK <yemengzhidiandig@gmail.com>
2021-01-26 16:31:40 +0800

    1、[优化]部分工具类

---
+++ utils-tools/src/main/java/com/chua/utils/tools/table/Table.java
@@ -0,0 +1,74 @@
+package com.chua.utils.tools.table;
+
+/**
+ * 表
+ *
+ * @author CH* @since 2021/1/25
+ * @version 1.0.0
+ */
+public interface Table {
+    /**
+     * 构构建表
+     *
+     * @return 表
+     */
+    String create();
+
+    /**
+     * 结束通知
+     */
+    default void notice() {
+
+    }
+
+    /**
+     * 构建
+     */
+    interface Builder<T, D> {
+        /**
+         * 表名
+         *
+         * @param name 表名
+         * @return this
+         */
+        Builder table(String name);
+
+        /**
+         * 表名
+         *
+         * @param source 表名
+         * @return this
+         */
+        Builder source(D source);
+
+        /**
+         * 字段
+         *
+         * @param columnName 名称
+         * @param columnType 类型
+         * @return this
+         */
+        Builder column(String columnName, String columnType);
+
+        /**
+         * 字段
+         *
+         * @param columnName 名称
+         * @param columnType 类型
+         * @return this
+         */
+        default Builder columns(String[] columnName, String columnType) {
+            for (String name : columnName) {
+                column(name, columnType);
+            }
+            return this;
+        }
+
+        /**
+         * 构建
+         *
+         * @return T
+         */
+        T build();
+    }
+}
