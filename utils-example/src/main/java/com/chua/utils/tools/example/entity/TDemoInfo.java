6772845eb55403bdde68c4d44080fc42b94b8092
ACHTK <yemengzhidiandig@gmail.com>
2021-01-27 17:41:14 +0800

    1、[新增]scala

--- utils-example/src/main/java/com/chua/utils/tools/example/entity/TDemoInfo.java
+++ utils-example/src/main/java/com/chua/utils/tools/example/entity/TDemoInfo.java
@@ -33,4 +33,59 @@ public class TDemoInfo {
 
     private String uuid = "1";
 
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public Date getTime() {
+        return time;
+    }
+
+    public void setTime(Date time) {
+        this.time = time;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDemoTitle() {
+        return demoTitle;
+    }
+
+    public void setDemoTitle(String demoTitle) {
+        this.demoTitle = demoTitle;
+    }
+
+    public String getTest() {
+        return test;
+    }
+
+    public void setTest(String test) {
+        this.test = test;
+    }
+
+    public String getUuid() {
+        return uuid;
+    }
+
+    public void setUuid(String uuid) {
+        this.uuid = uuid;
+    }
 }
