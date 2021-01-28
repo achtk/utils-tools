b7ab45cc75001a6ac58fc501c052335f5c6a8146
ACHTK <yemengzhidiandig@gmail.com>
2021-01-28 09:55:59 +0800

    1、[新增]#script

--- utils-tools/src/main/java/com/chua/utils/tools/logger/LogUtils.java
+++ utils-tools/src/main/java/com/chua/utils/tools/logger/LogUtils.java
@@ -1,19 +1,19 @@
 package com.chua.utils.tools.logger;
 
 import com.chua.utils.tools.common.DateHelper;
+import com.chua.utils.tools.expression.BshExpression;
+import com.chua.utils.tools.expression.Expression;
 import com.chua.utils.tools.function.converter.Converter;
 import com.chua.utils.tools.function.converter.TypeConverter;
 import com.chua.utils.tools.util.ArrayUtils;
 import com.chua.utils.tools.util.ClassUtils;
 import com.chua.utils.tools.util.CollectionUtils;
+import com.chua.utils.tools.util.SystemUtil;
 import com.google.common.base.Splitter;
 import lombok.extern.slf4j.Slf4j;
 
 import java.lang.reflect.Method;
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 /**
  * 日志工具
@@ -25,6 +25,7 @@ import java.util.Set;
 @Slf4j
 public class LogUtils {
     private static final CharSequence NULL = "null";
+    private static final String PID = SystemUtil.getPid();
 
     /**
      * 输出日志
@@ -48,6 +49,28 @@ public class LogUtils {
     public static void println(String message, Object... args) {
         message = message.replace("{}", "%s");
         System.out.println(String.format(message, args));
+    }
+
+    /**
+     * 输出日志{@link System#out#println(String)}
+     *
+     * @param script 信息
+     * @param env    环境
+     */
+    public static void script(String script, Map<String, Object> env) {
+        Expression expression = new BshExpression();
+        Optional.ofNullable(env).orElse(Collections.emptyMap()).entrySet().stream().forEach(entry -> {
+            try {
+                expression.set(entry.getKey(), entry.getValue());
+            } catch (Exception ignore) {
+            }
+        });
+        try {
+            Object eval = expression.eval(script.replace("'", "\""));
+            System.out.println("pid:(" + PID + ")> " + script + " -> " + eval);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     /**
@@ -107,6 +130,7 @@ public class LogUtils {
 
     /**
      * Info日志
+     *
      * @param message 信息
      */
     public static void info(String message) {
